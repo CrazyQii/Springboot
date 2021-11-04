2a20b3c0a68ff0d7ef880584bf71593a0c6b11a6
CrazyQii <858590598@qq.com>
2021-11-04 13:41:59 +0800

    update

--- Kafka/src/main/java/Kafka.java
+++ Kafka/src/main/java/Kafka.java
@@ -1,3 +1,27 @@
-public class Kafka {
+import javafx.concurrent.Worker;
+import org.apache.kafka.clients.producer.ProducerRecord;
 
+import java.util.Random;
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.SynchronousQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+public class Kafka {
+    private static final String TOPIC = "TEST";
+
+    public static void main(String[] args) {
+
+        ThreadPoolExecutor task = new ThreadPoolExecutor(1, 2147483, 60, TimeUnit.MINUTES, new SynchronousQueue<Runnable>());
+//        SimpleConsumerOld consumerOld = new SimpleConsumerOld(TOPIC);
+//        consumerOld.run();
+//        task.execute(consumerOld);
+
+
+        SimpleProducer producer = new SimpleProducer(TOPIC);
+        producer.run();
+
+        SimpleConsumerOld consumerOld = new SimpleConsumerOld(TOPIC);
+        consumerOld.run();
+    }
 }
--- Kafka/src/main/java/SimpleConsumer.java
+++ Kafka/src/main/java/SimpleConsumer.java
@@ -9,60 +9,56 @@ import java.util.Properties;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+/**
+ * 消费者新版本
+ */
 public class SimpleConsumer {
 
-    private final static String TOPIC = "TOPIC-MAIN";
-
-
-    public static void main(String[] args) {
-        // 创建消费者配置
-        Properties props = new Properties();
-
-        props.put("bootstrap.servers", "localhost:2181");
+//    private final static String TOPIC = "TEST";
+//
+//    public static void main(String[] args) {
+//        // 创建消费者配置
+//        Properties props = new Properties();
+//
+//        // Kafka地址
+//        props.put("bootstrap.servers", "172.20.102.102:9092");
 //
 //        //group 代表一个消费组
+//        // 组名 不同组名可以重复消费。例如你先使用了组名A消费了kafka的1000条数据，
+//        // 但是你还想再次进行消费这1000条数据，并且不想重新去产生，那么这里你只需要更改组名就可以重复消费了。
 //        props.put("group.id", "jd-group");
+//        // 是否自动提交，默认为true
 //        props.put("enable.auto.commit", "true");
 //        props.put("auto.commit.interval.ms", "1000");
 //
-//        //zk连接超时
-//        props.put("auto.offset.reset", "earliest");
+//        // zk连接超时
 //        props.put("session.timeout.ms", "30000");
-//        //反序列化
-        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-
-//        props.put("zookeeper.connect", "127.0.0.1:2182");
-
-        //group 代表一个消费组
-        props.put("group.id", "test-consumer-group");
-
-        //zk连接超时
-        props.put("zookeeper.session.timeout.ms", "4000");
-        props.put("zookeeper.sync.time.ms", "200");
-        props.put("auto.commit.interval.ms", "1000");
-        props.put("auto.offset.reset", "earliest");//必须要加，如果要读旧数据
-        //序列化类
-//        props.put("serializer.class", "kafka.serializer.StringEncoder");
-
-        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
-
-        consumer.subscribe(Collections.singleton(TOPIC));
-
-        try {
-            while (true) {
-                ConsumerRecords<String, String> consumerRecords = consumer.poll(100);
-
-                for (ConsumerRecord<String, String> record: consumerRecords) {
-                    System.out.println("topic: " + record.topic() + "消息:" + record.offset() + record.value());
-                }
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        } finally {
-            consumer.close();
-        }
-
-    }
+//        // 消费规则
+//        // earliest: 当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，从头开始消费 。
+//        // latest: 当各分区下有已提交的offset时，从提交的offset开始消费；无提交的offset时，消费新产生的该分区下的数据 。
+//        // none: topic各分区都存在已提交的offset时，从offset后开始消费；只要有一个分区不存在已提交的offset，则抛出异常。
+//        props.put("auto.offset.reset", "earliest");
+//        // 反序列化
+//        props.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//        props.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+//
+//
+//        KafkaConsumer<String, String> consumer = new KafkaConsumer<String, String>(props);
+//        consumer.subscribe(Collections.singletonList(TOPIC));
+//        try {
+//            while (true) {
+//                ConsumerRecords<String, String> records = consumer.poll(100);
+//                for (ConsumerRecord<String, String> record: records) {
+//                    System.out.println("消息: " + record.offset() + record.value());
+////                    System.out.println("key"  + record.key());
+//                }
+//            }
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//        } finally {
+//            consumer.close();
+//        }
+//
+//    }
 
 }
