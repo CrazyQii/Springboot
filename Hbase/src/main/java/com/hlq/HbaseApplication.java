427c0c2ad937c937ced5efe972ec08aee1acd55e
CrazyQii <858590598@qq.com>
2021-12-31 17:19:51 +0800

    update hbase

---
+++ Hbase/src/main/java/com/hlq/HbaseApplication.java
@@ -0,0 +1,11 @@
+package com.hlq;
+
+/**
+ * @program: HbaseApplication
+ * @description: Hbase Java Api
+ * @author: hanLinQi
+ * @create: 2021-12-31 16:16
+ **/
+
+public class HbaseApplication {
+}
---
+++ Hbase/src/main/java/com/hlq/dao/StudentDao.java
@@ -0,0 +1,9 @@
+package com.hlq.dao;
+
+/**
+ * @program: StudentDao
+ * @author: hanLinQi
+ * @create: 2021-12-31 17:14
+ **/
+public interface StudentDao {
+}
