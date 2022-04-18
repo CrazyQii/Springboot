d1cd81136f6f4425d9dcd5a0ff4a543e7250d791
CrazyQii <858590598@qq.com>
2022-04-18 18:28:03 +0800

    update

---
+++ mult-thread/src/main/java/com/hlq/service/CoreThread.java
@@ -0,0 +1,15 @@
+package com.hlq.service;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+/**
+ * @program: CoreThread
+ * @description:
+ * @author: hanLinQi
+ * @create: 2022-04-18 18:24
+ **/
+
+public class CoreThread {
+
+    ThreadPoolExecutor executor = new ThreadPoolExecutor();
+}
